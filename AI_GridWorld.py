53390998075590961e0aa9a17c6c3ccc9793448d
Alexander Anker Christensen <alexander.Anker.chr@gmail.com>
2021-11-21 17:07:08 +0100

    Added function for the statistics

--- AI_GridWorld.py
+++ AI_GridWorld.py
@@ -3,9 +3,25 @@ import numpy as np
 import pygame
 from collections import defaultdict
 
+# Function for calculating statistics
+def Statistics(data, sampleSize,t):
+    # Calculate the mean
+    mean = np.mean(data)
+
+    # Calculate the standard deviation
+    std = np.std(data)
+
+    # Calculate the confidence interval
+    confidenceInterval = t * std / np.sqrt(sampleSize)
+
+    # Print the calculated values
+    print("Mean: ", mean)
+    print("Standard deviation: ", std)
+    print("Confidence interval: ", mean, "+/-", confidenceInterval)
+
 # Values
-samples = 10
-score_threshold = 100
+sampleSize = 385
+score_target = 120
 
 gamma = 0.9
 epsilon = 0.1
@@ -29,7 +45,7 @@ clock = pygame.time.Clock()
 iteration_max = 1000
 iteration = 0
 sample = 0
-for i in range(samples):
+for i in range(sampleSize):
     # Reset and define data structure for q-table
     q_table = defaultdict(lambda: np.zeros(len(actions))) # Create table with default value of 0 for each action
     while iteration < iteration_max:            
@@ -56,7 +72,7 @@ for i in range(samples):
 
         # Log the score and iteration
         if done:
-            if score > score_threshold:
+            if score > score_target:
                 print("Sample: ", sample + 1, "Iteration: ", iteration + 1, "Score: ", score)
                 iteration_array = np.append(iteration_array, iteration + 1)
                 iteration = 0
@@ -67,9 +83,7 @@ for i in range(samples):
             score = 0
             iteration += 1
 
-# Print the iteration array to file
-with open("AI_GridWorld_QLearning_data.txt", "w") as file:
-    for i in iteration_array:
-        file.write("%d\n" % i)
-        
+# Run the statistics function
+Statistics(iteration_array, sampleSize, 1.96)
+
 env.close()
